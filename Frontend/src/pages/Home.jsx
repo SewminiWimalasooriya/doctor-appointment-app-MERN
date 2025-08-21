60936b0ae3985396653e7ac894babbbb4c20413c
SewminiWimalasooriya <sewminiwimalasooriya@gmail.com>
2025-08-21 15:02:06 +0530

    create account banner component

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import ToDoctors from '../components/ToDoctors'
+import Banner from '../components/Banner'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Header />
       <SpecialityMenu />
       <ToDoctors />
+      <Banner/>
     </div>
   )
 }
