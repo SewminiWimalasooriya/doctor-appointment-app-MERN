aca3a7089eb3c85407dd5e59be121d8b107064c6
SewminiWimalasooriya <sewminiwimalasooriya@gmail.com>
2025-08-21 13:42:46 +0530

    Add doctors List and crate context file

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
+import ToDoctors from '../components/ToDoctors'
 
 function Home() {
   return (
     <div>
       <Header />
       <SpecialityMenu />
+      <ToDoctors />
     </div>
   )
 }
