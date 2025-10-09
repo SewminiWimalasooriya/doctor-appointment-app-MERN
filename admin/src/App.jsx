92b35ad5f29fe717ad4cce9ad670990604fdfcbb
SewminiWimalasooriya <sewminiwimalasooriya@gmail.com>
2025-10-09 13:33:30 +0530

    useing aToken give access Admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import Login from './pages/Login'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AdminContext } from './context/AdminContextProvider';
+import { useContext } from 'react';
 
-export default function App() {
-  return (
+
+ const App = () =>{
+
+  const { aToken} = useContext(AdminContext)
+
+
+  return aToken ? (
     <div>
-      <Login/>
+      <ToastContainer/>  {/*aToken when available not display login*/}
     </div>
+  ):(
+    <>
+      <Login/>
+      <ToastContainer/>
+    </>
   )
 }
+
+export default App
--- admin/src/context/AdminContextProvider.jsx
+++ admin/src/context/AdminContextProvider.jsx
@@ -1,11 +1,19 @@
 import { createContext } from "react";
+import { useState } from "react";
 
 export const AdminContext = createContext()
 
 const AdminContextProvider = (props) => {
 
+    const [aToken,setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
     // value is the data you want to share via the context.
     const value = {
+        aToken,
+        setAToken,
+        backendUrl,
 
     }
 
