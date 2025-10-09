9856bd6760e70c3abd56a43f371f2c102541c1d8
SewminiWimalasooriya <sewminiwimalasooriya@gmail.com>
2025-10-09 20:22:13 +0530

    sidebar with dashboard

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,14 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContextProvider';
 import { useContext } from 'react';
+import Navbar from './components/Navbar';
+import Slidebar from './components/Slidebar';
+import { Routes } from 'react-router-dom';
+import AllAppointments from './pages/Admin/AllAppointments';
+import { Route } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import DoctorsList from './pages/Admin/DoctorsList';
+import AddDoctor from './pages/Admin/AddDoctor';
 
 
  const App = () =>{
@@ -12,8 +20,20 @@ import { useContext } from 'react';
 
 
   return aToken ? (
-    <div>
+    <div className='bg-[#F8F9FD]'>
       <ToastContainer/>  {/*aToken when available not display login*/}
+      <Navbar/>
+      <div className='flex items-start'>
+        <Slidebar/>
+        <Routes>
+          <Route path='/' element={<></>}/>  {/*default route*/}
+          <Route path='/admin-dashboard' element={<Dashboard/>}/>
+          <Route path='/all-appointments' element={<AllAppointments/>}/>
+          <Route path='/doctors-list' element={<DoctorsList/>}/>
+          <Route path='/add-doctor' element={<AddDoctor/>}/>
+          
+        </Routes>
+      </div>
     </div>
   ):(
     <>
